f0b91d33cca23f5bcf80a89cc361c42edfabdf62
John Ostap <ostap.john@gmail.com>
2020-05-27 16:52:16 -0700

    Added a loop to add each word to a function when it finds a SPACE in the .lyrics doc

--- app.js
+++ app.js
@@ -16,17 +16,36 @@ app.get('/', (req, res) => {
 });
 
 
+//When the user inputs a url, it searches Genius.com for the lyrics and scrapes them
 app.post('/search', (req, res) => {
-  console.log('POST')
-  console.log(req.body)
+  //GET the url
   axios.get(req.body.songUrl)
     .then(function (response) {
-      console.log('Then')
+      //When a song is found, use Cheerio to find the .lyrics div and get all
+      // the contents inside of it.
       const data = response.data;
       const $ = cheerio.load(data);
-      const doc = $('.lyrics').text().replace(/ *\[[^\]]*]/g, '')
-      // let final = doc.replace(/ *\[[^\]]*]/g, '');
-      // console.log(final)
+      //this gets the lyrics, uses regex to find any words that have brackets in them
+      // eg [Verse] or [Chorus] etc
+      const doc = $('.lyrics').text().replace(/ *\[[^\]]*]/g, '');
+
+      // words is an array, which gets .push() to when the loop finds a space key
+      // currentWord is the word the loop is going through.
+      let words = [];
+      let currentWord = '';
+
+      //Go through each letter in the lyrics. Append each letter to currentWord
+      // If the loop finds a space key, it pushed currentWord to the words variable
+      for(i=0; i<doc.length; i++) {
+        if(doc[i] === " ") {
+          words.push(currentWord);
+          currentWord = ''
+        } else {
+          currentWord += doc[i];
+        }
+      }
+      console.log(`Doc array length ${doc.length}`);
+      console.log(`Words array length ${words.length}`);
       return res.send(doc)
     })
     .catch(function (error) {
